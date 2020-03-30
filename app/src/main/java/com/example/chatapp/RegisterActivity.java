230a3ae285998eafe9ad1e788bf63e6f87843eed
Ermek01 <ermek.joldoshbekov.kg@gmail.com>
2020-03-31 04:07:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/RegisterActivity.java
@@ -0,0 +1,114 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private MaterialEditText username, email, password;
+    private Button btn_register;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        setupToolbar();
+        setupViews();
+        auth = FirebaseAuth.getInstance();
+        setupListeners();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Регистрация");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupListeners() {
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_username = username.getText().toString().trim();
+                String txt_email = email.getText().toString().trim();
+                String txt_password = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+                    Toast.makeText(RegisterActivity.this, "Заполните данные!!!", Toast.LENGTH_SHORT).show();
+                }
+                else if (txt_password.length() < 6){
+                    Toast.makeText(RegisterActivity.this, "Пароль должен быть не менее 6 символов", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    register(txt_username, txt_email, txt_password);
+                }
+            }
+        });
+    }
+
+    private void setupViews() {
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_register = findViewById(R.id.btn_register);
+    }
+
+    private void register(final String username, String email, String password){
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            assert firebaseUser != null;
+                            String userId = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userId);
+                            hashMap.put("username", username);
+                            hashMap.put("imageURL", "default");
+
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this, "Вы не можете зарегистрироваться с этим адресом электронной почты или паролем", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
